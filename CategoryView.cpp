0e67f9d54a56c8322b4da3c624c28f355ee2eef1
hari <hariharasudhan@live.com>
2016-02-20 10:48:54 +0530

    added lingerie

--- CategoryView.cpp
+++ CategoryView.cpp
@@ -32,7 +32,7 @@ CategoryView::CategoryView(gint rows, gint columns) {
 	bgImages[10] = "images/Icons/offer2.png";
 	bgImages[11] = "images/Icons/lifestyle.jpg";
 	bgImages[12] = "images/Icons/offer3.png";
-	bgImages[13] = "images/brands/apparel/fab.jpg";
+	bgImages[13] = "images/Icons/lingerie.jpg";
 	bgImages[14] = "images/brands/apparel/levis.png";
 	char* logoPath = "images/logo.png";
 
